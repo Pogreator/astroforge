9204bd62780cd8d3026ff37ffc4506e65f93104d
Pogreator <Pogreator@gmail.com>
2026-09-06 16:34:07 +0100

    Initial Commit
    Change data structure
    Large rewrite

--- scripts/Data/ChunkData.cs
+++ scripts/Data/ChunkData.cs
@@ -1,18 +1,29 @@
+using System.IO.IsolatedStorage;
 using Godot;
+
+public struct VoxelData
+{
+	byte Iso { get; set; }
+	ushort Type { get; set; }
+	short Health { get; set; }
+
+	public VoxelData(byte iso, ushort type, short health)
+	{
+		Iso = iso;
+		Type = type;
+		Health = health;
+	}
+}
 
 public class ChunkData
 {
 	public Vector3I LocalPosition { get; private set; }
-	public byte[] VoxelIso { get; set; }
-	public uint[] VoxelType { get; set; }
-	public int[] VoxelHealth { get; set; }
+	public VoxelData[] Voxels { get; set; }
 	public bool IsDirty { get; set; }
 
 	public ChunkData(Vector3I position, int size)
 	{
 		LocalPosition = position;
-		VoxelIso = new byte[size * size * size];
-		VoxelType = new uint[size * size * size];
-		VoxelHealth = new int[size * size * size];
+		Voxels = new VoxelData[size * size * size];
 	}
 }
